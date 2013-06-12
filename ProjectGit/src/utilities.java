053d9eb500f25af1c971c02d86a85b41171a8cd1
macostas <macostas@gmail.com>
2013-06-12 17:42:38 -0300

    add methods to start, stop and get video from the grid

--- ProjectGit/src/utilities.java
+++ ProjectGit/src/utilities.java
@@ -4,6 +4,11 @@ import static org.monte.media.VideoFormatKeys.*;
 import java.awt.GraphicsConfiguration;
 import java.awt.GraphicsEnvironment;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 import org.monte.media.Format;
@@ -51,16 +56,79 @@ public class utilities {
 	}
 
 	public static void showErrors(WebDriver driver, String msg) {
-		String myscript = "div = document.createElement(\"div\"); " 
+		String myscript = "div = document.createElement(\"div\"); "
 				+ " div.style.position = \"relative\"; "
 				+ " div.style.zindex = \"9999\"; "
 				+ " div.style.color = \"blue\"; "
 				+ " div.style.fontSize = \"34px\"; "
 				+ " div.style.backgroundcolor = \"white\"; "
-				+ " div.innerHTML = \"" + msg
+				+ " div.innerHTML = \""
+				+ msg
 				+ "\"; 		document.body.insertBefore(div, document.body.firstChild);";
 		((JavascriptExecutor) driver).executeScript(myscript);
 
+	}
+
+	public static void startVideoInGrid(String gridServer) {
+		String url = "http://" + gridServer
+				+ ":4455/RemoteVideoServer/videoServer?action=start";
+		try {
+
+			// creating the URL
+			URL u = new URL(url);
+
+			// openning a connection
+			URLConnection uCon = u.openConnection();
+			uCon.connect();
+			uCon.getInputStream();
+			System.out.println("start video");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void stopVideoInGrid(String gridServer) {
+		String url = "http://" + gridServer
+				+ ":4455/RemoteVideoServer/videoServer?action=stop";
+		try {
+
+			// creating the URL
+			URL u = new URL(url);
+
+			// openning a connection
+			URLConnection uCon = u.openConnection();
+			uCon.connect();
+			uCon.getInputStream();
+			System.out.println("start video");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void saveVideoFromGrid(String gridServer, String videoPath) {
+		String url = "http://" + gridServer
+				+ ":4455/RemoteVideoServer/videoServer?action=stop";
+		try {
+			URL u = new URL(url);
+			URLConnection uCon = u.openConnection();
+			uCon.connect();
+
+			InputStream input = uCon.getInputStream();
+			byte[] buffer = new byte[4096];
+			int n = -1;
+
+			OutputStream output = new FileOutputStream(videoPath);
+			while ((n = input.read(buffer)) != -1) {
+				if (n > 0) {
+					output.write(buffer, 0, n);
+				}
+			}
+			output.close();
+
+			System.out.println("get video");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
